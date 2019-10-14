26c045adbb2fd5177c178f97e521e9c04eff5fc7
timcknowles <tim.c.knowles@gmail.com>
2019-10-14 17:16:54 +0100

    change fab to toolbar version

--- static/fab.js
+++ static/fab.js
@@ -1,12 +1,7 @@
+
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.fixed-action-btn');
   var instances = M.FloatingActionButton.init(elems, {
-    direction: 'top',
-    hoverEnabled: false
+    toolbarEnabled: true
   });
 });
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.tooltipped');
-    var instances = M.Tooltip.init(elems, options);
-  });
